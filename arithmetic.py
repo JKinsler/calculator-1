017e3c0362189b6e3b912b7ee66e9fef622012e0
Your Name <you@example.com>
2020-01-08 12:28:06 -0800

    add_mult function added

--- arithmetic.py
+++ arithmetic.py
@@ -50,3 +50,9 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     
     return (num1 % num2)
+
+
+def add_mult(num1, num2, num3):
+    """Adds num1 and num2, and multiplies sum by num3 """
+
+    return (num1 + num2) * num3
